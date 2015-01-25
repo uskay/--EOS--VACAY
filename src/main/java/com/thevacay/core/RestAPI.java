0c8453aa3d62e832e0a544ba953f24747032a83f
uskay <uskay23@gmail.com>
2015-01-26 08:24:27 +0900

    V0.01

--- src/main/java/com/thevacay/core/RestAPI.java
+++ src/main/java/com/thevacay/core/RestAPI.java
@@ -1,22 +1,27 @@
 package com.thevacay.core;
 
+import com.thevacay.service.session.SessionBean;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by uskay on 1/24/15.
  */
 public class RestAPI {
 
-    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public IResponse get() {
+    @RequestMapping(value = "/session/{resource}", method = { RequestMethod.GET, RequestMethod.POST }, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public IResponse session(@PathVariable String resource, @ModelAttribute SessionBean bean, HttpServletRequest rawRequest) {
+
+        String httpMethod = rawRequest.getMethod();
+
         return null;
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public IResponse post() {
-        return null;
-    }
+
 
 }
--- src/main/java/com/thevacay/service/session/SessionService.java
+++ src/main/java/com/thevacay/service/session/SessionService.java
@@ -2,19 +2,22 @@ package com.thevacay.service.session;
 
 import com.thevacay.core.IRequest;
 import com.thevacay.core.IResponse;
+import com.thevacay.core.ServiceAPI;
 
 /**
  * Created by uskay on 1/25/15.
  */
-public class SessionServiceFacade {
+public class SessionService {
 
-    public IResponse getSessionID(IRequest req) {
+    @ServiceAPI(path = "session/token", httpMethod = "GET")
+    public IResponse getSessionToken(IRequest req) {
         SessionBean bean = (SessionBean) req;
 
 
         return null;
     }
 
+    @ServiceAPI(path = "session/operation", httpMethod = "POST")
     public IResponse logOperation(IRequest req) {
         return null;
     }

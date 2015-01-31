d53745456aba994c012e34281f88ad0ff165e9f6
uskay <uskay23@gmail.com>
2015-01-31 22:40:48 +0900

    V0.01

--- src/main/java/com/thevacay/core/HttpSessionConfig.java
+++ src/main/java/com/thevacay/core/HttpSessionConfig.java
@@ -12,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+/**
+ * You need to build Redis environment in order to use this class
+ */
 @Configuration
 @EnableRedisHttpSession
 public class HttpSessionConfig {
